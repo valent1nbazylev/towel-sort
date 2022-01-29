9e34aad8fba36202710b9bbba74dc656cb652c4c
valent1n <valent1n.bazylev@gmail.com>
2022-01-29 14:13:32 +0300

    towel sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,81 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+ let elementReverse, element
+	res = []
+	if (arguments.length == 0) { //если параметр не был передан
+		return res//вернуть пустую
+	} else {
+
+		for (let index = 0; index < matrix.length; index++) { 
+			element = matrix[index];
+
+			if (index % 2 == 0) { //если четный индекс
+				res.push(...element) //пушим в масив (...объединяет матрицу в массив)
+				// console.log('+итерация',res)
+			} else {
+
+				elementReverse = element.reverse() //иначе переворачиваем и пушим
+
+				res.push(...elementReverse) 
+				//  console.log('+итерация',res)
+			}
+		}
+		console.log('итог', res)
+		return res
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//========================================================================================
+/*towelSort = (matrix) => {
+	 let elementReverse, element
+	res = []
+	if (arguments.length == 0) { //если параметр не был передан
+		return res//вернуть пустую
+	} else {
+
+		for (let index = 0; index < matrix.length; index++) { 
+			element = matrix[index];
+
+			if (index % 2 == 0) { //если четный индекс
+				res.push(...element) //пушим в масив (...объединяет матрицу в массив)
+				// console.log('+итерация',res)
+			} else {
+
+				elementReverse = element.reverse() //иначе переворачиваем и пушим
+
+				res.push(...elementReverse) 
+				//  console.log('+итерация',res)
+			}
+		}
+		console.log('итог', res)
+		return res
+	}
+}
+
+towelSort(
+	[
+		[1, 2, 3],
+		[4, 5, 6],
+		[7, 8, 9],
+	])
+ */
